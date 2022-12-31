fbd73eba6b2c63a9276028e1c7ebca8d7cf0c1ba
shernof <sherwin.nofuente@yahoo.com>
2022-12-31 18:45:23 +1100

    video mobile

--- src/Components/jolVideo/JolVideo.jsx
+++ src/Components/jolVideo/JolVideo.jsx
@@ -1,8 +1,13 @@
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import JOL from "./JOL.mp4";
+import classes from "./jolvideo.module.css";
 
 export default function JolVideo() {
+  // const videoRef = useRef(undefined);
+  // useEffect(() => {
+  //   videoRef.current.defaultMuted = true;
+  // });
   return (
     <Box
       sx={{
@@ -12,10 +17,14 @@ export default function JolVideo() {
     >
       <video
         src={JOL}
-        autoPlay
-        loop
-        muted
-        playsInline
+        // className={classes.vid}
+        // src={props.video}
+        // ref={videoRef}
+        // type="video/mp4"
+        autoPlay={true}
+        loop={true}
+        muted={true}
+        playsInline={true}
         style={{
           width: "100%",
           height: "100vh",
