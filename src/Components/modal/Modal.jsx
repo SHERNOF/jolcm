96ce22a261c319fb395164cd7790455da651a7fe
shernof <sherwin.nofuente@yahoo.com>
2023-01-30 00:13:48 +1100

    modal

--- src/Components/modal/Modal.jsx
+++ src/Components/modal/Modal.jsx
@@ -1,11 +1,19 @@
 import classes from "./modal.module.css";
 import React, { useState } from "react";
 
-export default function Modal({ setShowModal, isOpen, isOpenClickHandler }) {
+export default function Modal({ isOpen, isOpenClickHandler }) {
   return (
     <>
-      <div className={classes.modalContainer} onClick={isOpenClickHandler}>
-        <div className={classes.modalContent}>JESUS IS LORD</div>
+      <div
+        className={`${classes.modalContainer} ${isOpen ? classes.appear : ""}`}
+        onClick={isOpenClickHandler}
+      >
+        <div
+          className={classes.modalContent}
+          onClick={(e) => e.stopPropagation()}
+        >
+          JESUS IS LORD
+        </div>
       </div>
     </>
   );
