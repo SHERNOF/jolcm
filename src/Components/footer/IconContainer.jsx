ccde137a6f0005f51e8713750bebad234b78daf3
shernof <sherwin.nofuente@yahoo.com>
2023-08-02 21:44:48 +1000

    1

--- src/Components/footer/IconContainer.jsx
+++ src/Components/footer/IconContainer.jsx
@@ -3,20 +3,7 @@ import classes from './footer.module.css'
 
 export default function IconContainer(props) {
   return (
-    <div className={classes.individualIcon}
-      style={{
-        // borderRadius: "50%",
-        // width: "2.5rem",
-        // height: "2.5rem",
-        // display: "flex",
-        // alignItems: "center",
-        // justifyContent: "center",
-        // padding: "2rem, 2rem",
-        // border: "1px solid rgb(83, 83, 85)",
-        // boxShadow: "0px 0px 15px #3776ff",  
-        // animation: 'all 5s'  
-      }}
-    >
+    <div className={classes.individualIcon}>
       {props.children}
     </div>
   );
