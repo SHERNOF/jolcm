5b5b45a21c974f568e267d74db1534620b52812f
shernof <sherwin.nofuente@yahoo.com>
2023-10-27 00:57:14 +1100

    started the carousel

--- src/Components/events/Events.jsx
+++ src/Components/events/Events.jsx
@@ -5,13 +5,28 @@ import Title from "../../UI/title/Title";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+
 export default function Events({ evs }) {
   const churchEvents = evs;
-  console.log(churchEvents);
 
   const [indexNum, setindexNum] = useState(0);
   console.log(indexNum);
-  const [bigPicFromSlider, setbigPicFromSlider] = useState(0);
+
+  const slides = churchEvents[indexNum].pictures.length;
+  const pics = churchEvents[indexNum].pictures;
+  console.log(typeof slides);
+  console.log(pics);
+
+  const previous = () => {
+    const isSlide1 = indexNum === 0;
+    const newIndex = isSlide1 ? slides.length - 1 : indexNum - 1;
+    setindexNum(newIndex);
+  };
+  const next = () => {
+    const isLastSlide = indexNum === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : slides.length + 1;
+    setindexNum(newIndex);
+  };
 
   return (
     <div className={classes.events}>
@@ -21,7 +36,7 @@ export default function Events({ evs }) {
           <div className={classes.column1Container}>
             {churchEvents.map((churchEvent, index) => (
               <div
-                key={churchEvent.eventNumber}
+                key={index}
                 className={classes.eventsLineup}
                 onClick={() => {
                   setindexNum(index);
@@ -44,22 +59,27 @@ export default function Events({ evs }) {
         </div>
         <div className={classes.column2}>
           <div className={classes.column2Container}>
-            <div className={classes.arrowRight}>
+            <div className={classes.arrowRight} onClick={next}>
               <KeyboardArrowRightIcon />
             </div>
-            <div className={classes.arrowLeft}>
+            <div className={classes.arrowLeft} onClick={previous}>
               <KeyboardArrowLeftIcon />
             </div>
-            {churchEvents[indexNum].pictures.map((pics, index) => (
-              <div
-                // ref={divRef}
-                className={`${classes.mainPic} ${classes.fade}`}
-                key={index}
-              >
-                <img src={pics} alt="events" className={classes.bigImages} />
-                {/* <Overlay></Overlay> */}
-              </div>
-            ))}
+
+            {/* {churchEvents[indexNum].pictures.map((pic, index) => ( */}
+            <div
+              className={`${classes.sliderContainer} ${classes.fade}`}
+              // key={index}
+            >
+              {pics.map((pic, index) => (
+                <img
+                  key={index}
+                  src={pic}
+                  alt="events"
+                  className={classes.bigImages}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
@@ -68,3 +88,11 @@ export default function Events({ evs }) {
 }
 
 /* requested by the member to make the big picture to enlarge upon click. decided to included as a modal in the backdrop */
+{
+  /* <div
+                  key={index}
+                  style={{ backgroundImage: `url(${pic[index]})` }}
+                ></div> */
+}
+
+https://www.youtube.com/watch?v=SK9AlIbexOE&list=LL&index=3
