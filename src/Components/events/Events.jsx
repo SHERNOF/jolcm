14248e04bf88849caa80426e9576de0fb890d37a
shernof <sherwin.nofuente@yahoo.com>
2023-10-29 02:41:42 +1100

    test the carousel

--- src/Components/events/Events.jsx
+++ src/Components/events/Events.jsx
@@ -1,33 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import classes from "./events.module.css";
 import Overlay from "../../UI/overlay/Overlay";
 import Title from "../../UI/title/Title";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-
-import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
-import ImageSlider from "./ImageSlider";
+import ImageSlider from "../../UI/imageSlider/ImageSlider";
 
 export default function Events({ evs }) {
   const churchEvents = evs;
 
   const [indexNum, setindexNum] = useState(0);
-  console.log(indexNum);
 
-  const slides = churchEvents[indexNum].pictures.length;
   const pics = churchEvents[indexNum].pictures;
-  console.log(slides);
-  console.log(typeof pics);
-
-  const previous = () => {
-    const isSlide1 = indexNum === 0;
-    const newIndex = isSlide1 ? slides.length - 1 : indexNum - 1;
-    setindexNum(newIndex);
-  };
-  const next = () => {
-    const isLastSlide = indexNum === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : slides.length + 1;
-    setindexNum(newIndex);
-  };
 
   return (
     <div className={classes.events}>
@@ -50,7 +32,7 @@ export default function Events({ evs }) {
                 </div>
                 <img
                   loading="lazy"
-                  src={churchEvent.pictures[0]}
+                  src={churchEvent.pictures[2]}
                   alt="events"
                   className={classes.eventImage}
                 ></img>
@@ -60,30 +42,7 @@ export default function Events({ evs }) {
         </div>
         <div className={classes.column2}>
           <div className={classes.column2Container}>
-            {/* <div className={classes.arrowRight} onClick={next}>
-              <KeyboardArrowRightIcon />
-            </div>
-            <div className={classes.arrowLeft} onClick={previous}>
-              <KeyboardArrowLeftIcon />
-            </div> */}
-
-            {/* {churchEvents[indexNum].pictures.map((pic, index) => ( */}
-            {/* <div
-              className={`${classes.sliderContainer} ${classes.fade}`}
-              key={index}
-            >
-              {pics.map((pic, index) => (
-                <img
-                  key={index}
-                  src={pic}
-                  alt="events"
-                  className={classes.bigImages}
-                />
-              ))}
-            </div> */}
-            <div className={classes.containerStyles}>
             <ImageSlider slides={pics} />
-            </div>
           </div>
         </div>
       </div>
---
+++ src/UI/imageSlider/ImageSlider.jsx
@@ -0,0 +1,179 @@
+import { useState } from "react";
+import styled from "@emotion/styled";
+
+import { BiCircle } from "react-icons/bi";
+import { FaCircle } from "react-icons/fa";
+import {
+  IoIosArrowDropleftCircle,
+  IoIosArrowDroprightCircle,
+} from "react-icons/io";
+
+const slideContainer = {
+  width: "100%",
+  height: "100%",
+  position: "relative",
+  margin: "0 auto",
+};
+
+const RightArrowStyles = styled("div")`
+  all: unset;
+  display: block;
+  position: absolute;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 3;
+  height: 100%;
+  width: 35px;
+  padding: 0.5rem;
+  font-size: 25px;
+  top: 50%;
+  transform: translate(0, -50%);
+
+  transition: all 0.5s;
+  background: none;
+  transition: background-color 200ms ease-in-out;
+
+  &:hover {
+    color: white;
+    background-color: rgb(0, 0, 0, 0.2);
+    height: 100%;
+    right: 0;
+    cursor: pointer;
+    animation: test 0.5s ease-in-out;
+  }
+
+  @keyframes test {
+    50% {
+      scale: 1.3 1;
+    }
+  }
+`;
+
+const LeftArrowStyles = {
+  ...RightArrowStyles,
+};
+
+const dotsContainerStyles = {
+  display: "flex",
+  justifyContent: "center",
+  position: "absolute",
+  bottom: "3%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  transition: "all 1s",
+};
+const Button = styled.button`
+  cursor: pointer;
+  display: block;
+  all: unset;
+  width: 1rem;
+  height: 1rem;
+  font-size: 1rem;
+  transition: all 0.25s;
+  margin-left: 0.25rem;
+
+  & {
+    cursor: pointer;
+    height: 100%;
+    width: 100%;
+  }
+  &:hover {
+    scale: 1.25;
+  }
+`;
+
+const Image = styled("img")`
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  flex-shrink: 0;
+  flex-grow: 0;
+  transition: all 0.25s ease-in-out;
+`;
+
+const ImageSlider = ({ slides }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  return (
+    <div style={slideContainer}>
+      <RightArrowStyles onClick={goToNext} style={{ right: "1px" }}>
+        <IoIosArrowDroprightCircle />
+      </RightArrowStyles>
+      <LeftArrowStyles onClick={goToPrevious} style={{ left: "1px" }}>
+        <IoIosArrowDropleftCircle />
+      </LeftArrowStyles>
+
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "relative",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            height: "100%",
+            overflow: "hidden",
+          }}
+        >
+          {slides.map((slide) => (
+            <Image
+              key={slide}
+              style={{
+                translate: `${-100 * currentIndex}%`,
+              }}
+              src={slide}
+              alt="fellowship"
+            />
+          ))}
+        </div>
+      </div>
+      <div style={dotsContainerStyles}>
+        {slides.map((slide, slideIndex) => (
+          <Button key={slideIndex} onClick={() => goToSlide(slideIndex)}>
+            {slideIndex === currentIndex ? (
+              <FaCircle style={{ color: "#000000fd", fill: "white" }} />
+            ) : (
+              <BiCircle style={{ color: "white" }} />
+            )}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
+
+/*
+Updates: 
+1. used spread operator to utilize RightArrowStyles properties to LefhtArrowStyles.
+
+2. Use React Icons to control the appearance of circle buttons depending on the state
+
+3. use of flex-grow and flex-shrink to 0, to ensure all image will be the same size of container
+
+
+
+*/
